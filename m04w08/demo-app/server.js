d2a1282dba7151c07d0ae994dadaa3b6e5c49d2a
Andy Lindsay <andydlindsay@gmail.com>
2023-06-06 09:26:20 -0700

    changes

--- m04w08/demo-app/server.js
+++ m04w08/demo-app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const port = 8080;
+const port = 8001;
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
